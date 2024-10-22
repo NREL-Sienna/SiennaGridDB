98f06568660c0901f1c5684d2d42b81691254ec2
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2024-10-22 13:18:05 -0600

    Move to hand-defined schemas

--- sienna_database_gen/database_load.py
+++ sienna_database_gen/database_load.py
@@ -1,42 +1,23 @@
 from sqlalchemy import Connection, create_engine, insert, select
 from .sqlalchemy_schemas import metadata_obj, balancing_topology, attributes
 from .pydantic_schemas import (
-    OBJ_SUBCLASSES,
     GenerateJSONSchemaWithSQLInfo,
     GenerationUnit,
     ObjModel,
-    ACBus,
 )
 
 engine = create_engine("sqlite:///:memory:")
 metadata_obj.create_all(engine)
 
 
-acbus = ACBus(id=0, name="Bus 1", number=1)
 
-
-def load_to_db(conn: Connection, data: ObjModel):
-    json_data = data.model_dump(mode="JSON")
-    row = {}
-    for col in data.get_columns():
-        row[col.name] = json_data[col.name]
-        del json_data[col.name]
-
-    conn.execute(insert(balancing_topology).values(row))
-    conn.execute(
-        insert(attributes).values(
-            [
-                {
-                    "entity_id": data.id,
-                    "entity_type": "balancing_topology",
-                    "key": key,
-                    "value": value,
-                }
-                for key, value in json_data.items()
-            ]
-        )
-    )
-    return data.id
+def load_to_db(conn: Connection, data):
+    # Parse data to JSON
+    # Ascertain type
+    # Pack into main row
+    # Create attribute JSON
+    # Insert into database
+    return inserted_id
 
 
 def load_from_db(conn: Connection, table, id):
@@ -50,14 +31,19 @@ def load_from_db(conn: Connection, table, id):
             attributes.c.entity_id == id, attributes.c.entity_type == table.name
         )
     ).fetchall()
+
+
     full_json = {
         **{key: value for key, value in zip(entity_result.keys(), row)},
         **{key: value for key, value in entity_attributes},
     }
 
+    # Parse data into obj
     data = OBJ_SUBCLASSES[full_json["obj_type"]].parse_obj(full_json)
     return data
 
+
+# acbus = ???
 
 with engine.begin() as conn:
     load_to_db(conn, acbus)
