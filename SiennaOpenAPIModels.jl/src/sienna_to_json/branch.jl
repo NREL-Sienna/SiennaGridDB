7ed3678f68ad817f7aeac0343ae20616fccc10d0
Chubin <hchubin@nrel.gov>
2025-01-29 14:38:11 -0800

    added Transformer2W to src/sienna_to_json/branch.jl

--- SiennaOpenAPIModels.jl/src/sienna_to_json/branch.jl
+++ SiennaOpenAPIModels.jl/src/sienna_to_json/branch.jl
@@ -14,3 +14,19 @@ function psy2openapi(line::PSY.Line, ids::IDGenerator)
         g = get_from_to(line.g),
     )
 end
+
+function psy2openapi(transformer2w::PSY.transformer2w, ids::IDGenerator)
+    Transformer2W(
+        id = getid!(ids, transformer2w),
+        name = transformer2w.name,
+        available = transformer2w.available,
+        active_power_flow = transformer2w.active_power_flow * PSY.get_base_power(transformer2w),
+        reactive_power_flow = transformer2w.reactive_power_flow * PSY.get_base_power(transformer2w),
+        arc = getid!(ids, transformer2w.arc),
+        r = transformer2w.r,
+        x = transformer2w.x,
+        rating = transformer2w.rating * PSY.get_base_power(transformer2w),
+        primary_shunt = PSY.get_primary_shunt(transformer2w.primary_shunt),
+    )
+end
+
