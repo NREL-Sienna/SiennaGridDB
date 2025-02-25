51ca471d9b3ae6a2ec0546d345de8b481cb6fb6c
Pradyumna Rao <56094134+prao7@users.noreply.github.com>
2025-02-25 13:06:30 -0700

    feat: json function trial (#4)
    * database connected to schema, needs populating
    
    * correcting transmission lines for schema and adding gitignore to remove the processing files for db.
    
    * Schema edited to pull in transmission, generation and load information. ATB not yet decided
    
    * added piecewise draft and supply technologies draft
    
    * corrected schema for data type syntax error
    
    * corrections from pedro, new prime mover type and piecewise linear functions.
    
    * correcting the piecewise to add the capacity steps
    
    * schema updated with the piecewise linear blob and time series blob
    
    * v1.0.0 schema, dummy data still incomplete
    
    * dummy_data updated for new schema
    
    * new db with operational data
    
    * schema removing piecewise_linear as a table and updating time series and entity handling
    
    * removing the time series blob due to storage concerns with the blob.
    
    * key for time series table changed to reflect the entity reference
    
    * schema comments resolved
    
    * corrections to schema to avoid syntax errors
    
    ---------
    
    Co-authored-by: Pradyumna Rao <pradyumna.rao@nrel.gov>
    Co-authored-by: pesap <pesap@users.noreply.github.com>

--- schema.sql
+++ schema.sql
@@ -6,6 +6,7 @@ drop view if exists unit_time_series;
 drop table if exists prime_mover_types;
 drop table if exists balancing_topologies;
 drop table if exists supply_technologies;
+drop table if exists storage_technologies;
 drop table if exists transmission_lines;
 drop table if exists demand_requirements;
 drop table if exists areas;
@@ -14,6 +15,10 @@ drop table if exists data_types;
 drop table if exists time_series;
 drop table if exists piecewise_linear;
 drop table if exists transmission_interchange;
+drop table if exists entities;
+drop table if exists linkages;
+drop table if exists reserves;
+drop table if exists operational_data;
 
 -- only generation units
 create table  generation_units (
@@ -22,6 +27,7 @@ create table  generation_units (
 	prime_mover text not null,
 	fuel_type text not null,
 	balancing_topology text not null references balancing_topologies(name),
+	start_year integer not null check (start_year >= 0),
 	rating float not null check (rating > 0 ),
 	base_power float not null check (base_power > 0),
 	check (base_power >= rating),
@@ -37,25 +43,53 @@ create table  storage_units (
 	max_capacity float not null check (max_capacity > 0),
 	round_trip_efficiency float check (round_trip_efficiency >= 0),
 	balancing_topology text not null references balancing_topologies(name),
-	rating float not null check (rating > 0 ),
+	charging_efficiency float not null check (charging_efficiency > 0),
+	discharge_efficiency float not null check (discharge_efficiency > 0),
+	start_year integer not null check (start_year >= 0),
+	rating float not null default 1 check (rating > 0 ),
 	base_power float not null check (base_power > 0),
 	scenario text null,
 	check (base_power >= rating),
 	foreign key (prime_mover, fuel_type) references prime_mover_types(prime_mover, fuel_type)
 );
 
--- create table for technologies
+-- create table for technologies, perhaps change the name to investment_technologies
 create table supply_technologies (
     technology_id integer primary key,
     prime_mover text not null,
-	fuel_type text not null,
-	technology_class real null,
+	  fuel_type text not null,
+	  technology_class real null,
     vom_cost float not null check (vom_cost >= 0),
     fom_cost float not null check (fom_cost >= 0),
     scenario text null,
+	  area text null references areas(name),
+	  balancing_topology text null references balancing_topologies(name),
+	  foreign key (prime_mover, fuel_type) references prime_mover_types(prime_mover, fuel_type)
+);
+
+create table storage_technologies (
+	storage_unit_id integer primary key,
+	name text not null unique,
+	prime_mover text not null,
+	fuel_type text not null,
+	scenario text null,
 	area text null references areas(name),
 	balancing_topology text null references balancing_topologies(name),
 	foreign key (prime_mover, fuel_type) references prime_mover_types(prime_mover, fuel_type)
+);
+
+
+create table operational_data (
+	unit_id references generation_units(unit_id),
+	fom_cost float not null check (fom_cost >= 0),
+	vom_cost float not null check (vom_cost >=0),
+	forced_outage float not null,
+	startup_cost float not null check (startup_cost >= 0),
+	min_stable_level float not null check (min_stable_level >= 0),
+	mttr integer not null check (mttr >= 0),
+	startup_fuel_mmbtu_per_mw float not null check (startup_fuel_mmbtu_per_mw >= 0),
+	uptime float not null check (uptime >= 0),
+	downtime float not null check (downtime >= 0)
 );
 
 -- create table for prime movers
@@ -128,12 +162,60 @@ create table data_types(
 	description text null
 );
 
+create table reserves(
+    id integer primary key,
+    time_frame float not null,
+    requirement float not null,
+    direction text not null
+);
+
+-- Entities gets populated automatically once there is an insert on the core tables.
+create table entities(
+	id integer primary key,
+	entity_type text not null references table_names(name),
+	entity_id integer not null
+);
+
+
+
+CREATE TRIGGER IF NOT EXISTS autofill_supply
+AFTER INSERT ON supply_technologies
+BEGIN
+    INSERT INTO entities(entity_type, entity_id) VALUES("supply_technologies", new.technology_id);
+END;
+
+CREATE TRIGGER IF NOT EXISTS autofill_generation_units
+AFTER INSERT ON generation_units
+BEGIN
+	INSERT INTO entities(entity_type, entity_id) VALUES("generation_units", new.unit_id);
+END;
+
+CREATE TRIGGER IF NOT EXISTS autofill_storage_units
+AFTER INSERT ON storage_units
+BEGIN
+	INSERT INTO entities(entity_type, entity_id) VALUES("storage_units", new.storage_unit_id);
+END;
+
+CREATE TRIGGER IF NOT EXISTS autofill_storage_technologies
+AFTER INSERT ON storage_technologies
+BEGIN
+	INSERT INTO entities(entity_type, entity_id) VALUES("storage_technologies", new.storage_unit_id);
+END;
+
+CREATE TRIGGER IF NOT EXISTS autofill_reserves
+AFTER INSERT ON reserves
+BEGIN
+    INSERT INTO entities(entity_type, entity_id) VALUES("reserves", new.id);
+END;
+
 -- Create function data table
 
 insert into data_types (name, validation_query) values
 ('integer', 'cast(? as integer) is not null'),
+('float', 'cast(? as float) is not null'),
 ('real', 'cast(? as real) is not null'),
 ('text', 'cast(? as text) is not null'),
+('json', 'json_valid(?) is 1');
 ('time_series', '? is null'),
 ('piecewise_linear', '? is null');
 -- Create a new data type for function data here
@@ -148,32 +230,21 @@ begin
     select case
         when new.data_type = 'integer' and typeof(new.value) != 'integer' then
             raise(fail, 'invalid data type for attribute value. expected integer.')  -- noqa: PRS
-        when new.data_type = 'time_series' and typeof(new.value) != null then
-            raise(fail, 'invalid data type for attribute value. expected real.')  -- noqa: PRS
         when new.data_type = 'text' and typeof(new.value) != 'text' then
             raise(fail, 'invalid data type for attribute value. expected text.')  -- noqa: PRS
-		when new.data_type = 'piecewise_linear' and typeof(new.value) != null then
-			raise(fail, 'invalid data type for attribute value. expected real.')  -- noqa: PRS
+		when new.data_type = 'float' and typeof(new.value) != 'float' then
+			raise(fail, 'invalid data type for attribute value. expected float.')  -- noqa: PRS
+		when new.data_type = 'json' and json_valid(new.value) != 1 then
+            raise(fail, 'invalid data type for attribute value. expected valid JSON.') -- noqa: PRS
     -- add more conditions for other data types as needed
     end;
 end;
 
 
 create table time_series(
-	entity_attribute_id integer references attributes(entity_attribute_id),
-    time_series_blob blob,
-    timestamp int as (time_series_blob ->> 'timestamp'),
-    value float as (time_series_blob ->> 'value')
-);
-
-
-create table piecewise_linear(
-	entity_attribute_id integer references attributes(entity_attribute_id),
-	piecewise_linear_blob blob,
-	from_x float as (piecewise_linear_blob ->> 'from_x'),
-	to_x float as (piecewise_linear_blob ->> 'to_x'),
-	from_y float as (piecewise_linear_blob ->> 'from_y'),
-	to_y float as (piecewise_linear_blob ->> 'to_y')
+	entity_id integer references entities(id),
+	timestamp not null,
+	value float not null
 );
 
 create view unit_attributes as
