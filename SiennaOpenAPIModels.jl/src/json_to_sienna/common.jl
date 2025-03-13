71ab07e58c90ff4d31599eddd03fc7a6984b3a1f
Chubin <hchubin@nrel.gov>
2025-03-13 16:43:20 -0700

    Still WIP, but added openapi2psy for ThermalStandard, and a handful of functions to json_to_sienna/common.jl

--- SiennaOpenAPIModels.jl/src/json_to_sienna/common.jl
+++ SiennaOpenAPIModels.jl/src/json_to_sienna/common.jl
@@ -14,8 +14,108 @@ function get_bustype_enum(bustype::String)
     end
 end
 
+function get_prime_mover_enum(prime_mover::String)
+    if prime_mover == "BA"
+        return PSY.PrimeMovers.BA
+    elseif prime_mover == "BT"
+        return PSY.PrimeMovers.BT
+    elseif prime_mover == "CA"
+        return PSY.PrimeMovers.CA
+    elseif prime_mover == "CC"
+        return PSY.PrimeMovers.CC
+    elseif prime_mover == "CE"
+        return PSY.PrimeMovers.CE
+    elseif prime_mover == "CP"
+        return PSY.PrimeMovers.CP
+    elseif prime_mover == "CS"
+        return PSY.PrimeMovers.CS
+    elseif prime_mover == "CT"
+        return PSY.PrimeMovers.CT
+    elseif prime_mover == "ES"
+        return PSY.PrimeMovers.ES
+    elseif prime_mover == "FC"
+        return PSY.PrimeMovers.FC
+    elseif prime_mover == "FW"
+        return PSY.PrimeMovers.FW
+    elseif prime_mover == "GT"
+        return PSY.PrimeMovers.GT
+    elseif prime_mover == "HA"
+        return PSY.PrimeMovers.HA
+    elseif prime_mover == "HB"
+        return PSY.PrimeMovers.HB
+    elseif prime_mover == "HK"
+        return PSY.PrimeMovers.HK
+    elseif prime_mover == "HY"
+        return PSY.PrimeMovers.HY
+    elseif prime_mover == "IC"
+        return PSY.PrimeMovers.IC
+    elseif prime_mover == "PS"
+        return PSY.PrimeMovers.PS
+    elseif prime_mover == "OT"
+        return PSY.PrimeMovers.OT
+    elseif prime_mover == "ST"
+        return PSY.PrimeMovers.ST
+    elseif prime_mover == "PVe"
+        return PSY.PrimeMovers.PVe
+    elseif prime_mover == "WT"
+        return PSY.PrimeMovers.WT
+    elseif prime_mover == "WS"
+        return PSY.PrimeMovers.WS
+    else
+        error("Unknown prime mover type: $(prime_mover)")
+    end
+end
+
+function get_fuel_type_enum(fuel_type::String)
+    if fuel_type == "COAL"
+        return PSY.ThermalFuels.COAL
+    elseif fuel_type == "WASTE_COAL"
+        return PSY.ThermalFuels.WASTE_COAL
+    elseif fuel_type == "DISTILLATE_FUEL_OIL"
+        return PSY.ThermalFuels.DISTILLATE_FUEL_OIL
+    elseif fuel_type == "WASTE_OIL"
+        return PSY.ThermalFuels.WASTE_OIL
+    elseif fuel_type == "PETROLEUM_COKE"
+        return PSY.ThermalFuels.PETROLEUM_COKE
+    elseif fuel_type == "RESIDUAL_FUEL_OIL"
+        return PSY.ThermalFuels.RESIDUAL_FUEL_OIL
+    elseif fuel_type == "NATURAL_GAS"
+        return PSY.ThermalFuels.NATURAL_GAS
+    elseif fuel_type == "OTHER_GAS"
+        return PSY.ThermalFuels.OTHER_GAS
+    elseif fuel_type == "NUCLEAR"
+        return PSY.ThermalFuels.NUCLEAR
+    elseif fuel_type == "AG_BIPRODUCT"
+        return PSY.ThermalFuels.AG_BIPRODUCT
+    elseif fuel_type == "MUNICIPAL_WASTE"
+        return PSY.ThermalFuels.MUNICIPAL_WASTE
+    elseif fuel_type == "WOOD_WASTE"
+        return PSY.ThermalFuels.WOOD_WASTE
+    elseif fuel_type == "GEOTHERMAL"
+        return PSY.ThermalFuels.GEOTHERMAL
+    elseif fuel_type == "OTHER"
+        return PSY.ThermalFuels.OTHER
+    else
+        error("Unknown thermal fuel type: $(fuel_type)")
+    end
+end
+
 function get_tuple_min_max(obj::MinMax)
     return (min=obj.min, max=obj.max)
+end
+
+function get_tuple_up_down(obj::UpDown)
+    return (up=obj.up, down=obj.down)
+end
+
+function get_thermal_cost(cost::ThermalGenerationCost)
+    PSY.ThermalGenerationCost(
+        cost_type="THERMAL",
+        start_up=get_startup(cost.start_up),
+        shut_down=cost.shut_down,
+        fixed=cost.fixed,
+        variable=ProductionVariableCostCurve(get_variable_cost(cost.variable)),
+    )
 end
 
 mutable struct Resolver
---
+++ SiennaOpenAPIModels.jl/src/json_to_sienna/static_injections.jl
@@ -0,0 +1,21 @@
+function openapi2psy(thermal::ThermalStandard, resolver::Resolver)
+    PSY.ThermalStandard(;
+        name=thermal.name,
+        prime_mover=get_prime_mover_enum(thermal.prime_mover_type),
+        fuel_type=get_fuel_type_enum(thermal.fuel_type),
+        rating=thermal.rating,
+        base_power=thermal.base_power,
+        available=thermal.available,
+        status=thermal.status,
+        time_at_status=thermal.time_at_status,
+        active_power=thermal.active_power,
+        reactive_power=thermal.reactive_power,
+        active_power_limits=get_tuple_from_min_max(thermal.active_power_limits),
+        reactive_power_limits=get_tuple_min_max(thermal.reactive_power_limits),
+        ramp_limits=get_tuple_up_down(thermal.ramp_limits),
+        operation_cost=get_thermal_cost(thermal.operation_cost),
+        time_limits=get_tuple_up_down(thermal.time_limits),
+        must_run=thermal.must_run,
+        bus=resolver(thermal.bus),
+    )
+end
