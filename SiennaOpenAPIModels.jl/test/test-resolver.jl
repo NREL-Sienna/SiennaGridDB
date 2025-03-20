f920017d4fcf7eb773a36dc0b3feb0e478927385
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2025-03-20 14:12:36 -0600

    Fix resolver with more testing

---
+++ SiennaOpenAPIModels.jl/test/test-resolver.jl
@@ -0,0 +1,11 @@
+@testset "Test Resolver" begin
+    sys =
+        PowerSystemCaseBuilder.build_system(PowerSystemCaseBuilder.PSISystems, "c_sys5_pjm")
+    component = PSY.get_component(PSY.ThermalStandard, sys, "Solitude")
+
+    id_generator = IDGenerator()
+    @test getid!(id_generator, component) == 1
+    resolver = SiennaOpenAPIModels.resolver_from_id_generator(id_generator, sys)
+
+    @test resolver(1) == component
+end
