e10ba9cad389589f53c5456d02c73adf602416fc
Chubin <hchubin@nrel.gov>
2025-03-27 14:45:30 -0700

    added renewable dispatch, rewrote fuel type getter to use IS.deserialize, all tests passing

--- SiennaOpenAPIModels.jl/src/json_to_sienna/static_injections.jl
+++ SiennaOpenAPIModels.jl/src/json_to_sienna/static_injections.jl
@@ -28,3 +28,25 @@ function openapi2psy(thermal::ThermalStandard, resolver::Resolver)
         bus=resolver(thermal.bus),
     )
 end
+
+function openapi2psy(renew::RenewableDispatch, resolver::Resolver)
+    if renew.base_power == 0.0
+        error("base power is 0.0")
+    end
+    PSY.RenewableDispatch(;
+        name=renew.name,
+        available=renew.available,
+        bus=resolver(renew.bus),
+        active_power=renew.active_power / renew.base_power,
+        reactive_power=renew.reactive_power / renew.base_power,
+        rating=renew.rating / renew.base_power,
+        prime_mover_type=get_prime_mover_enum(renew.prime_mover_type),
+        reactive_power_limits=divide(
+            get_tuple_min_max(renew.reactive_power_limits),
+            renew.base_power,
+        ),
+        power_factor=renew.power_factor,
+        operation_cost=get_sienna_renewable_cost(renew.operation_cost),
+        base_power=renew.base_power,
+    )
+end
