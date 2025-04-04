cdae1494bf20179e7e63e9d11a654f9201e9ace0
Chubin <hchubin@nrel.gov>
2025-04-04 14:04:34 -0700

    cleaned up sienna_to_json directory and all tests passing

--- SiennaOpenAPIModels.jl/src/sienna_to_json/topology.jl
+++ SiennaOpenAPIModels.jl/src/sienna_to_json/topology.jl
@@ -27,15 +27,6 @@ function psy2openapi(area::PSY.Area, ids::IDGenerator)
     )
 end
 
-function psy2openapi(load_zone::PSY.LoadZone, ids::IDGenerator)
-    LoadZone(
-        id=getid!(ids, load_zone),
-        name=load_zone.name,
-        peak_active_power=load_zone.peak_active_power * PSY.get_base_power(load_zone),
-        peak_reactive_power=load_zone.peak_reactive_power * PSY.get_base_power(load_zone),
-    )
-end
-
 function psy2openapi(dcbus::PSY.DCBus, ids::IDGenerator)
     DCBus(
         id=getid!(ids, dcbus),
@@ -46,5 +37,14 @@ function psy2openapi(dcbus::PSY.DCBus, ids::IDGenerator)
         base_voltage=dcbus.base_voltage,
         area=getid!(ids, dcbus.area),
         load_zone=getid!(ids, dcbus.load_zone),
+    )
+end
+
+function psy2openapi(load_zone::PSY.LoadZone, ids::IDGenerator)
+    LoadZone(
+        id=getid!(ids, load_zone),
+        name=load_zone.name,
+        peak_active_power=load_zone.peak_active_power * PSY.get_base_power(load_zone),
+        peak_reactive_power=load_zone.peak_reactive_power * PSY.get_base_power(load_zone),
     )
 end
