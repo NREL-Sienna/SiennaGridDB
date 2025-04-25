e7297ebff50f20b2df5a5d0293f31da0a0963561
Joseph McKinsey <joseph.mckinsey@nrel.gov>
2025-04-25 14:30:43 -0600

    Reformat with sqltools instead

--- dummy_data.sql
+++ dummy_data.sql
@@ -1,38 +1,28 @@
 PRAGMA foreign_keys = ON;
 
-INSERT INTO
-    prime_mover_types (name, description)
-VALUES
-    ('CT', "[C]ombustion [T]urbine"),
+INSERT INTO prime_mover_types (name, description)
+VALUES ('CT', "[C]ombustion [T]urbine"),
     ('HY', "[HY]droelectric"),
     ('WT', "[W]ind [T]urbine"),
     ('BA', "Energy Storage, [BA]ttery"),
     ('PS', "[P]ump [S]torage"),
     ('PV', "[P]hoto[V]oltaic");
 
-INSERT INTO
-    fuels(name, description)
-VALUES
-    ("NG", "[N]atural [G]as"),
+INSERT INTO fuels(name, description)
+VALUES ("NG", "[N]atural [G]as"),
     ("Oil", "Oil");
 
-INSERT INTO
-    entities (id, entity_type)
-VALUES
-    (0, 'planning_regions'),
+INSERT INTO entities (id, entity_type)
+VALUES (0, 'planning_regions'),
     (1, 'planning_regions');
 
 -- Areas are the higher level aggregation of balancing topologies
-INSERT INTO
-    planning_regions (id, name, description)
-VALUES
-    (0, 'North', 'Northern region'),
+INSERT INTO planning_regions (id, name, description)
+VALUES (0, 'North', 'Northern region'),
     (1, 'South', 'Southern region');
 
-INSERT INTO
-    entities (id, entity_type)
-VALUES
-    (2, 'balancing_topologies'),
+INSERT INTO entities (id, entity_type)
+VALUES (2, 'balancing_topologies'),
     (3, 'balancing_topologies'),
     (4, 'balancing_topologies'),
     (5, 'balancing_topologies'),
@@ -42,10 +32,8 @@ VALUES
     (9, 'balancing_topologies');
 
 -- Balancing topologies are the lower level aggregation of generation units
-INSERT INTO
-    balancing_topologies (id, name, area, description)
-VALUES
-    (
+INSERT INTO balancing_topologies (id, name, area, description)
+VALUES (
         2,
         'load_area_01',
         0,
@@ -94,17 +82,14 @@ VALUES
         'Commercial area with generation from wind and storage'
     );
 
-INSERT INTO
-    entities (id, entity_type)
-VALUES
-    (10, 'generation_units'),
+INSERT INTO entities (id, entity_type)
+VALUES (10, 'generation_units'),
     (11, 'generation_units'),
     (12, 'generation_units'),
     (13, 'generation_units');
 
 -- Inserting data for generation units
-INSERT INTO
-    generation_units (
+INSERT INTO generation_units (
         id,
         name,
         prime_mover,
@@ -113,22 +98,18 @@ INSERT INTO
         rating,
         base_power
     )
-VALUES
-    (10, 'Unit 1', 'CT', 'NG', 6, 1, 200),
+VALUES (10, 'Unit 1', 'CT', 'NG', 6, 1, 200),
     (11, 'Unit 2', 'HY', NULL, 7, 1, 300),
     (12, 'Unit 3', 'PV', NULL, 8, 1, 200),
     (13, 'Unit 4', 'WT', NULL, 9, 1, 200);
 
-INSERT INTO
-    entities (id, entity_type)
-VALUES
-    (14, 'storage_units'),
+INSERT INTO entities (id, entity_type)
+VALUES (14, 'storage_units'),
     (15, 'storage_units'),
     (16, 'storage_units');
 
 -- Inserting data for storage units
-INSERT INTO
-    storage_units (
+INSERT INTO storage_units (
         id,
         name,
         prime_mover,
@@ -138,8 +119,7 @@ INSERT INTO
         rating,
         base_power
     )
-VALUES
-    (
+VALUES (
         14,
         'Storage Unit 2',
         "PS",
@@ -170,38 +150,27 @@ VALUES
         300
     );
 
-INSERT INTO
-    entities (id, entity_type)
-VALUES
-    (17, 'arcs'),
+INSERT INTO entities (id, entity_type)
+VALUES (17, 'arcs'),
     (18, 'arcs'),
     (19, 'arcs');
 
 -- Insert some arcs
-INSERT INTO
-    arcs (id, from_id, to_id)
-VALUES
-    (17, 3, 4);
+INSERT INTO arcs (id, from_id, to_id)
+VALUES (17, 3, 4);
 
-INSERT INTO
-    arcs (id, from_id, to_id)
-VALUES
-    (18, 5, 4);
+INSERT INTO arcs (id, from_id, to_id)
+VALUES (18, 5, 4);
 
-INSERT INTO
-    arcs (id, from_id, to_id)
-VALUES
-    (19, 7, 8);
+INSERT INTO arcs (id, from_id, to_id)
+VALUES (19, 7, 8);
 
-INSERT INTO
-    entities (id, entity_type)
-VALUES
-    (20, 'transmission_lines'),
+INSERT INTO entities (id, entity_type)
+VALUES (20, 'transmission_lines'),
     (21, 'transmission_lines');
 
 -- Inserting data for transmission lines
-INSERT INTO
-    transmission_lines (
+INSERT INTO transmission_lines (
         id,
         name,
         arc_id,
@@ -210,8 +179,7 @@ INSERT INTO
         lte_rating,
         line_length
     )
-VALUES
-    (
+VALUES (
         20,
         "transmission_line1",
         18,
@@ -230,59 +198,46 @@ VALUES
         22.0
     );
 
-INSERT INTO
-    entities (id, entity_type)
-VALUES
-    (22, 'supply_technologies'),
+INSERT INTO entities (id, entity_type)
+VALUES (22, 'supply_technologies'),
     (23, 'supply_technologies'),
     (24, 'supply_technologies'),
     (25, 'supply_technologies');
 
 -- Inserting data for investment technologies
-INSERT INTO
-    supply_technologies (
+INSERT INTO supply_technologies (
         id,
         prime_mover,
         fuel,
         balancing_topology,
         scenario
     )
-VALUES
-    (22, "WT", NULL, "region_01", NULL),
+VALUES (22, "WT", NULL, "region_01", NULL),
     (23, "WT", NULL, "region_01", "Expensive"),
     (24, "CT", "NG", "region_01", "Expensive"),
     (25, "PV", NULL, "region_02", NULL);
 
-INSERT INTO
-    entities (id, entity_type)
-VALUES
-    (26, 'supplemental_attributes'),
+INSERT INTO entities (id, entity_type)
+VALUES (26, 'supplemental_attributes'),
     (27, 'supplemental_attributes');
 
 -- Supplemental attributes
-INSERT INTO
-    supplemental_attributes (id, TYPE, value)
-VALUES
-    (26, 'outage', json("[0,1,2,3]"));
+INSERT INTO supplemental_attributes (id, TYPE, value)
+VALUES (26, 'outage', json("[0,1,2,3]"));
 
-INSERT INTO
-    supplemental_attributes (id, TYPE, value)
-VALUES
-    (
+INSERT INTO supplemental_attributes (id, TYPE, value)
+VALUES (
         27,
         'geolocation',
         json("{'lat': 30.5, 'lon': -99.5}")
     );
 
 -- Add supplemental attribute to some entities
-INSERT INTO
-    supplemental_attributes_association (attribute_id, entity_id)
-VALUES
-    (26, 3);
+INSERT INTO supplemental_attributes_association (attribute_id, entity_id)
+VALUES (26, 3);
 
 -- Add time series examples
-INSERT INTO
-    time_series (
+INSERT INTO time_series (
         time_series_uuid,
         time_series_type,
         name,
@@ -293,8 +248,7 @@ INSERT INTO
         length,
         owner_id
     )
-VALUES
-    -- Hourly time series for a day (24 points)
+VALUES -- Hourly time series for a day (24 points)
     (
         '0',
         'SingleTimeSeries',
--- triggers.sql
+++ triggers.sql
@@ -1,262 +1,178 @@
 CREATE TRIGGER IF NOT EXISTS check_planning_regions_entity_exists BEFORE
-INSERT
-    ON planning_regions
-BEGIN
-SELECT
-    RAISE(
+INSERT ON planning_regions BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type planning_regions must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'planning_regions'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_balancing_topologies_entity_exists BEFORE
-INSERT
-    ON balancing_topologies
-BEGIN
-SELECT
-    RAISE(
+INSERT ON balancing_topologies BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type balancing_topologies must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'balancing_topologies'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_arcs_entity_exists BEFORE
-INSERT
-    ON arcs
-BEGIN
-SELECT
-    RAISE(
+INSERT ON arcs BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type arcs must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'arcs'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_transmission_lines_entity_exists BEFORE
-INSERT
-    ON transmission_lines
-BEGIN
-SELECT
-    RAISE(
+INSERT ON transmission_lines BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type transmission_lines must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'transmission_lines'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_transmission_interchanges_entity_exists BEFORE
-INSERT
-    ON transmission_interchanges
-BEGIN
-SELECT
-    RAISE(
+INSERT ON transmission_interchanges BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type transmission_interchanges must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'transmission_interchanges'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_generation_units_entity_exists BEFORE
-INSERT
-    ON generation_units
-BEGIN
-SELECT
-    RAISE(
+INSERT ON generation_units BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type generation_units must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'generation_units'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_storage_units_entity_exists BEFORE
-INSERT
-    ON storage_units
-BEGIN
-SELECT
-    RAISE(
+INSERT ON storage_units BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type storage_units must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'storage_units'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_hydro_reservoir_entity_exists BEFORE
-INSERT
-    ON hydro_reservoir
-BEGIN
-SELECT
-    RAISE(
+INSERT ON hydro_reservoir BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type hydro_reservoir must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'hydro_reservoir'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_supply_technologies_entity_exists BEFORE
-INSERT
-    ON supply_technologies
-BEGIN
-SELECT
-    RAISE(
+INSERT ON supply_technologies BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type supply_technologies must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'supply_technologies'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_transport_technologies_entity_exists BEFORE
-INSERT
-    ON transport_technologies
-BEGIN
-SELECT
-    RAISE(
+INSERT ON transport_technologies BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type transport_technologies must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'transport_technologies'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_supplemental_attributes_entity_exists BEFORE
-INSERT
-    ON supplemental_attributes
-BEGIN
-SELECT
-    RAISE(
+INSERT ON supplemental_attributes BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type supplemental_attributes must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'supplemental_attributes'
     );
 
 END;
 
 CREATE TRIGGER IF NOT EXISTS check_loads_entity_exists BEFORE
-INSERT
-    ON loads
-BEGIN
-SELECT
-    RAISE(
+INSERT ON loads BEGIN
+SELECT RAISE(
         ABORT,
         'Entity ID ' || NEW.id || ' with type loads must exist in entities table before insertion'
     )
-WHERE
-    NOT EXISTS (
-        SELECT
-            1
-        FROM
-            entities
-        WHERE
-            id = NEW.id
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM entities
+        WHERE id = NEW.id
             AND entity_type = 'loads'
     );
 
@@ -264,49 +180,33 @@ END;
 
 CREATE TRIGGER IF NOT EXISTS enforce_arc_entity_types_insert
 AFTER
-INSERT
-    ON arcs
-BEGIN
--- Fetch entity types for from_id and to_id and perform checks
-SELECT
-    CASE
+INSERT ON arcs BEGIN -- Fetch entity types for from_id and to_id and perform checks
+SELECT CASE
         -- Check if from_id entity type is valid
         WHEN (
-            SELECT
-                entity_type
-            FROM
-                entities
-            WHERE
-                id = NEW.from_id
+            SELECT entity_type
+            FROM entities
+            WHERE id = NEW.from_id
         ) NOT IN ('balancing_topologies', 'planning_regions') THEN RAISE(
             ABORT,
             'Invalid from_id entity type: must be balancing_topologies or planning_regions'
         ) -- Check if to_id entity type is valid
         WHEN (
-            SELECT
-                entity_type
-            FROM
-                entities
-            WHERE
-                id = NEW.to_id
+            SELECT entity_type
+            FROM entities
+            WHERE id = NEW.to_id
         ) NOT IN ('balancing_topologies', 'planning_regions') THEN RAISE(
             ABORT,
             'Invalid to_id entity type: must be balancing_topologies or planning_regions'
         ) -- Check if from_id and to_id entity types match
         WHEN (
-            SELECT
-                entity_type
-            FROM
-                entities
-            WHERE
-                id = NEW.from_id
+            SELECT entity_type
+            FROM entities
+            WHERE id = NEW.from_id
         ) != (
-            SELECT
-                entity_type
-            FROM
-                entities
-            WHERE
-                id = NEW.to_id
+            SELECT entity_type
+            FROM entities
+            WHERE id = NEW.to_id
         ) THEN RAISE(
             ABORT,
             'Entity types for from_id and to_id must match'
