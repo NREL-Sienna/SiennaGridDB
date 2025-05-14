2fad6fcb53ec3bb67da2e6d227ac37b1714e2f99
Chubin <hchubin@nrel.gov>
2025-05-14 16:31:12 -0700

    reviewed sienna_to_json multiplied parameters, need to get to json_to_sienna still

--- SiennaOpenAPIModels.jl/src/sienna_to_json/branch.jl
+++ SiennaOpenAPIModels.jl/src/sienna_to_json/branch.jl
@@ -3,8 +3,9 @@ function psy2openapi(area_interchange::PSY.AreaInterchange, ids::IDGenerator)
         id=getid!(ids, area_interchange),
         name=area_interchange.name,
         available=area_interchange.available,
-        active_power_flow=area_interchange.active_power_flow *
-                          PSY.get_base_power(area_interchange),
+        active_power_flow=(
+            area_interchange.active_power_flow * PSY.get_base_power(area_interchange)
+        ),
         flow_limits=get_fromto_tofrom(
             scale(area_interchange.flow_limits, PSY.get_base_power(area_interchange)),
         ),
@@ -18,13 +19,13 @@ function psy2openapi(line::PSY.Line, ids::IDGenerator)
         id=getid!(ids, line),
         name=line.name,
         available=line.available,
-        active_power_flow=line.active_power_flow * PSY.get_base_power(line),
-        reactive_power_flow=line.reactive_power_flow * PSY.get_base_power(line),
+        active_power_flow=(line.active_power_flow * PSY.get_base_power(line)),
+        reactive_power_flow=(line.reactive_power_flow * PSY.get_base_power(line)),
         arc=getid!(ids, line.arc),
         r=line.r,
         x=line.x,
         b=get_from_to(line.b),
-        rating=line.rating * PSY.get_base_power(line),
+        rating=(line.rating * PSY.get_base_power(line)),
         angle_limits=get_min_max(line.angle_limits),
         g=get_from_to(line.g),
     )
@@ -35,8 +36,8 @@ function psy2openapi(monitored::PSY.MonitoredLine, ids::IDGenerator)
         id=getid!(ids, monitored),
         name=monitored.name,
         available=monitored.available,
-        active_power_flow=monitored.active_power_flow * PSY.get_base_power(monitored),
-        reactive_power_flow=monitored.reactive_power_flow * PSY.get_base_power(monitored),
+        active_power_flow=(monitored.active_power_flow * PSY.get_base_power(monitored)),
+        reactive_power_flow=(monitored.reactive_power_flow * PSY.get_base_power(monitored)),
         arc=getid!(ids, monitored.arc),
         r=monitored.r,
         x=monitored.x,
@@ -44,7 +45,7 @@ function psy2openapi(monitored::PSY.MonitoredLine, ids::IDGenerator)
         flow_limits=get_fromto_tofrom(
             scale(monitored.flow_limits, PSY.get_base_power(monitored)),
         ),
-        rating=monitored.rating * PSY.get_base_power(monitored),
+        rating=(monitored.rating * PSY.get_base_power(monitored)),
         angle_limits=get_min_max(monitored.angle_limits),
         g=get_from_to(monitored.g),
     )
@@ -55,9 +56,10 @@ function psy2openapi(transformer::PSY.PhaseShiftingTransformer, ids::IDGenerator
         id=getid!(ids, transformer),
         name=transformer.name,
         available=transformer.available,
-        active_power_flow=transformer.active_power_flow * PSY.get_base_power(transformer),
-        reactive_power_flow=transformer.reactive_power_flow *
-                            PSY.get_base_power(transformer),
+        active_power_flow=(transformer.active_power_flow * PSY.get_base_power(transformer)),
+        reactive_power_flow=(
+            transformer.reactive_power_flow * PSY.get_base_power(transformer)
+        ),
         arc=getid!(ids, transformer.arc),
         r=transformer.r,
         x=transformer.x,
@@ -74,9 +76,10 @@ function psy2openapi(transformer::PSY.TapTransformer, ids::IDGenerator)
         id=getid!(ids, transformer),
         name=transformer.name,
         available=transformer.available,
-        active_power_flow=transformer.active_power_flow * PSY.get_base_power(transformer),
-        reactive_power_flow=transformer.reactive_power_flow *
-                            PSY.get_base_power(transformer),
+        active_power_flow=(transformer.active_power_flow * PSY.get_base_power(transformer)),
+        reactive_power_flow=(
+            transformer.reactive_power_flow * PSY.get_base_power(transformer)
+        ),
         arc=getid!(ids, transformer.arc),
         r=transformer.r,
         x=transformer.x,
@@ -91,7 +94,7 @@ function psy2openapi(tmodel::PSY.TModelHVDCLine, ids::IDGenerator)
         id=getid!(ids, tmodel),
         name=tmodel.name,
         available=tmodel.available,
-        active_power_flow=tmodel.active_power_flow * PSY.get_base_power(tmodel),
+        active_power_flow=(tmodel.active_power_flow * PSY.get_base_power(tmodel)),
         arc=getid!(ids, tmodel.arc),
         r=tmodel.r,
         l=tmodel.l,
@@ -110,14 +113,16 @@ function psy2openapi(transformer2w::PSY.Transformer2W, ids::IDGenerator)
         id=getid!(ids, transformer2w),
         name=transformer2w.name,
         available=transformer2w.available,
-        active_power_flow=transformer2w.active_power_flow *
-                          PSY.get_base_power(transformer2w),
-        reactive_power_flow=transformer2w.reactive_power_flow *
-                            PSY.get_base_power(transformer2w),
+        active_power_flow=(
+            transformer2w.active_power_flow * PSY.get_base_power(transformer2w)
+        ),
+        reactive_power_flow=(
+            transformer2w.reactive_power_flow * PSY.get_base_power(transformer2w)
+        ),
         arc=getid!(ids, transformer2w.arc),
         r=transformer2w.r,
         x=transformer2w.x,
-        rating=transformer2w.rating * PSY.get_base_power(transformer2w),
+        rating=scale(transformer2w.rating, PSY.get_base_power(transformer2w)),
         primary_shunt=transformer2w.primary_shunt,
     )
 end
@@ -127,7 +132,7 @@ function psy2openapi(hvdc::PSY.TwoTerminalGenericHVDCLine, ids::IDGenerator)
         id=getid!(ids, hvdc),
         name=hvdc.name,
         available=hvdc.available,
-        active_power_flow=hvdc.active_power_flow * PSY.get_base_power(hvdc),
+        active_power_flow=(hvdc.active_power_flow * PSY.get_base_power(hvdc)),
         arc=getid!(ids, hvdc.arc),
         active_power_limits_from=get_min_max(
             scale(hvdc.active_power_limits_from, PSY.get_base_power(hvdc)),
